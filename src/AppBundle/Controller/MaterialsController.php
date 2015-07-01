db38585d6a11a497193b85b4567c93a8540a3fe7
Alrick <alrick.deillon@unifr.ch>
2015-07-01 17:17:12 +0200

    new materials controller and view

--- src/AppBundle/Controller/MaterialsController.php
+++ src/AppBundle/Controller/MaterialsController.php
@@ -5,13 +5,13 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class MaterialsController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="materials")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('materials/index.html.twig');
     }
 }
