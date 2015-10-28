6ea654c83c49b1d1fac8dbca9fe9f7fd2c0b1d90
Alrick <alrick.deillon@unifr.ch>
2015-10-28 14:54:02 +0100

    rewrite materials javascript

--- web/js/materials.js
+++ web/js/materials.js
@@ -1,7 +1,5 @@
-// Define every Filters
-// var filtersMedia = $("#filtersMedia");
-// var filtersDate = $("#filtersDate");
-// var filtersLanguage = $("#filtersLanguage");
+var materialsTable;
+
 var fiLanguage      = $("#fiLanguage");
 var fiTheme         = $("#fiTheme");
 var fiModality      = $("#fiModality");
@@ -18,6 +16,26 @@ var fiSource        = $("#fiSource");
 var fiType          = $("#fiType");
 var fiYear          = $("#fiYear");
 
+var fiLanguageValue      = "";
+var fiThemeValue         = "";
+var fiModalityValue      = "";
+var fiVocabularyValue    = "";
+var fiReadingValue       = "";
+var fiListeningValue     = "";
+var fiSpeakingValue      = "";
+var fiPronunciationValue = "";
+var fiWritingValue       = "";
+var fiSpellingValue      = "";
+var fiGrammarValue       = "";
+var fiTestsValue         = "";
+var fiSourceValue        = "";
+var fiTypeValue          = "";
+var fiYearValue          = "";
+
+var THEME_COL       = 3;
+var LANGUAGE_COL    = 6;
+var MODALITY_COL    = 8;
+
 // Allow to format the lang level to include label class instead of pure text
 var formatLangLevel = function(data) {
     var formatted = "";
@@ -30,33 +48,90 @@ var filterTable = function(table, col, value) {
     value = value.toLowerCase() == "all" ? "" : value;
     table.columns(col).search(value).draw();
 };
-// used for date
-// $.fn.dataTable.ext.search.push(
-//     function(settings, data, dataIndex) {
-//
-//         // Filters medias
-//         var filtersMediaValue = filtersMedia.data("value");
-//         var media = data[filtersMedia.data("col")];
-//         //console.log(filtersMediaValue);
-//         //console.log(media);
-//
-//         // Filters date
-//         var filtersDateValue = parseInt(filtersDate.data("value"), 10);
-//         var year = parseFloat(data[filtersDate.data("col")]) || 0;
-//
-//         if (
-//             (isNaN(filtersDateValue) || filtersDateValue <= year) &&
-//             (isNaN(filtersMediaValue) || $.inArray(media, filtersMediaValue))
-//            )
-//         {
-//             return true;
-//         }
-//         return false;
-//     }
-// )
+
+var setThemeAlts = function(themes) {
+    if ($.inArray('reise', themes) != -1) {
+        themes.push("tourismus");
+    }
+    if ($.inArray('literatur', themes) != -1) {
+        themes.push("literaturwissenschaft");
+    }
+    if ($.inArray('gastronomie', themes) != -1) {
+        themes.push("hotellerie");
+    }
+    if ($.inArray('geisteswissenschaft', themes) != -1) {
+        themes.push("akademisch");
+        themes.push("wissenschaft");
+    }
+    if ($.inArray('naturwissenschaft', themes) != -1) {
+        themes.push("akademisch");
+        themes.push("wissenschaft");
+    }
+    if ($.inArray('informatik', themes) != -1) {
+        themes.push("internet");
+    }
+    if ($.inArray('medizin', themes) != -1) {
+        themes.push("pharmazie");
+    }
+    return themes;
+}
+
+var setFiltersValues = function() {
+    fiLanguageValue      = fiLanguage.chosen().val();
+    fiThemeValue         = fiTheme.chosen().val();
+    fiModalityValue      = fiModality.chosen().val();
+    fiVocabularyValue    = fiVocabulary.chosen().val();
+    fiReadingValue       = fiReading.chosen().val();
+    fiListeningValue     = fiListening.chosen().val();
+    fiSpeakingValue      = fiSpeaking.chosen().val();
+    fiPronunciationValue = fiPronunciation.chosen().val();
+    fiWritingValue       = fiWriting.chosen().val();
+    fiSpellingValue      = fiSpelling.chosen().val();
+    fiGrammarValue       = fiGrammar.chosen().val();
+    fiTestsValue         = fiTests.chosen().val();
+    fiSourceValue        = fiSource.chosen().val();
+    fiTypeValue          = fiType.chosen().val();
+    fiYearValue          = fiYear.chosen().val();
+}
+
+// Check each row to filter
+$.fn.dataTable.ext.search.push(
+    function(settings, data, dataIndex) {
+
+        // fiLanguage
+        if (data[LANGUAGE_COL] != fiLanguageValue) {
+            return false;
+        }
+
+        // fiTheme
+        var themes = setThemeAlts(data[THEME_COL].toLowerCase().split(" "));
+        var themesCheck = false;
+        // iterate over every themes values to check if there is correspondance
+        fiThemeValue.filter(function(n) {
+            themesCheck = (themes.indexOf(n) != -1) ? true : themesCheck;
+        });
+        if (!themesCheck) {
+            return false;
+        }
+
+        // fiModality
+        var modalities = data[MODALITY_COL].toLowerCase().split(" ");
+        var modalitiesCheck = false;
+        // iterate over every themes values to check if there is correspondance
+        fiModalityValue.filter(function(n) {
+            modalitiesCheck = (modalities.indexOf(n) != -1) ? true : modalitiesCheck;
+        });
+        if (!modalitiesCheck) {
+            return false;
+        }
+
+        return true;
+    }
+)
 
 
 $(document).ready(function() {
+
     // Datatable init
     materialsTable = $("#materials .table").DataTable({
         "select": true,
@@ -71,7 +146,8 @@ $(document).ready(function() {
             { "data": "lang_source[, ]" },
             { "data": "medium", "render": function (data, type, full, meta) { return data[0] } },
             { "data": "lang_target", "visible": false },
-            { "data": "year", "visible": false }
+            { "data": "year", "visible": false },
+            { "data": "asl", "visible": false }
         ],
         "language": {
             loadingRecords: '<div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%"><span class="sr-only">45% Complete</span></div></div>'
@@ -79,30 +155,15 @@ $(document).ready(function() {
     });
 
     // Chosen init
-    $(".js-chosen").chosen({width: "95%"});
+    chosenElem = $(".js-chosen");
+    chosenElem.chosen({width: "95%"});
 
-    // // Selectize init
-    // filtersLanguage.selectize({
-    //     onChange : function(value) {
-    //         filterTable(materialsTable, filtersLanguage.data("col"), value);
-    //     }
-    // });
-    //
-    // filtersMedia.selectize({
-    //     onChange : function(value) {
-    //         filtersMedia.data("value", value);
-    //         materialsTable.draw();
-    //     }
-    // });
-    //
-    // filtersDate.selectize({
-    //     onChange : function(value) {
-    //         filtersDate.data("value", value);
-    //         materialsTable.draw();
-    //     }
-    // });
-    //
-    // var filtersSource = $("#filtersSource");
-    // filtersSource.selectize({
-    // });
-} );
+    // Init filters values
+    setFiltersValues();
+
+    // Chosen on change
+    chosenElem.change(function () {
+        setFiltersValues();
+        materialsTable.draw();
+    });
+})
